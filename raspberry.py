0eaf7e369101bd2e57e2f28b28f7812bb1f98a4b
Eduardo Ramirez Torres <eramirez13@g.ucla.edu>
2022-10-09 01:11:46 +0100

    Step 3: Upload code w/o merge conflicts

---
+++ raspberry.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+	for i in range(2):
+		print("-------------------------------")
+	print("-  Print to console complete  -")
+	for i in range(2):
+		print("-------------------------------")
